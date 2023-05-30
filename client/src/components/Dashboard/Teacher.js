e574ce15947d65a886e6d6ed41efd23daac802f1
unknown <radovan.jorgic@viberate.com>
2023-05-31 00:48:29 +0200

    Added to know its reserved

--- client/src/components/Dashboard/Teacher.js
+++ client/src/components/Dashboard/Teacher.js
@@ -53,36 +53,49 @@ const Teacher = () => {
       <Box>
         {offers && offers.length > 0 ? (
           <Grid templateColumns="repeat(auto-fit, minmax(300px, 1fr))" gap={4}>
-            {offers.map((offer) => (
-              <Box
-                key={offer.id}
-                borderWidth="1px"
-                borderRadius="lg"
-                p={4}
-                boxShadow="sm"
-              >
-                <Box bg={"blue.300"} h={48} mb={4} />
-                <VStack position="relative" spacing={2} align="start">
-                  <CloseButton
-                    position="absolute"
-                    right={0}
-                    top={0}
-                    onClick={() => removeOffer(offer.id)}
-                  />
-                  <Heading as="h3" size="md">
-                    {offer.native_language} → {offer.learning_language}
-                  </Heading>
-                  <Text color="gray.500">Opis ({offer.username}):</Text>
-                  <Text>{offer.description}</Text>
+            {offers.map((offer) => {
+              const isReserved = offer.is_reserved != 0;
 
-                  <Text color="gray.500">Datum in čas:</Text>
+              return (
+                <Box
+                  key={offer.id}
+                  borderWidth="1px"
+                  borderRadius="lg"
+                  p={4}
+                  boxShadow="sm"
+                >
+                  <Box
+                    bg="blue.300"
+                    h={48}
+                    mb={4}
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                  >
+                    {isReserved && <Text color="white">Rezervirano!</Text>}
+                  </Box>
+                  <VStack position="relative" spacing={2} align="start">
+                    <CloseButton
+                      position="absolute"
+                      right={0}
+                      top={0}
+                      onClick={() => removeOffer(offer.id)}
+                    />
+                    <Heading as="h3" size="md">
+                      {offer.native_language} → {offer.learning_language}
+                    </Heading>
+                    <Text color="gray.500">Opis ({offer.username}):</Text>
+                    <Text>{offer.description}</Text>
 
-                  <Text>
-                    {`${offer.availability_date}, ${offer.availability_time}`}
-                  </Text>
-                </VStack>
-              </Box>
-            ))}
+                    <Text color="gray.500">Datum in čas:</Text>
+
+                    <Text>
+                      {`${offer.availability_date}, ${offer.availability_time}`}
+                    </Text>
+                  </VStack>
+                </Box>
+              );
+            })}
           </Grid>
         ) : (
           <Text>Ni dodanih ponudb.</Text>
